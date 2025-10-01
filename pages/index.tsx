00454760ad4b1eabd4791e623d991fbec5caff26
chakrit <chakritphatyensuk9@gmail.com>
2025-10-01 23:08:56 +0700

    remove pages directory, use app router only

--- pages/index.tsx
+++
@@ -1,8 +0,0 @@
-export default function Index() {
-  return (
-    <main style={{ padding: "40px", fontFamily: "sans-serif" }}>
-      <h1>✅ ReadNow Home (pages/index.tsx)</h1>
-      <p>If you see this, the site is working.</p>
-    </main>
-  );
-}
